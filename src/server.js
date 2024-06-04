c955e2f8ad87d51da46a1b09561215fa7dd655df
sam <155526957+Invinciblecode@users.noreply.github.com>
2024-06-04 15:03:20 +0100

    uploading project files

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import mongoose from 'mongoose'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+import ejs from 'ejs'
+import user from '../models/user.js'
+// user route
+import addUserRoute from "./routes/user/add-user.js"
+import editUserRoute from './routes/user/edit-user.js'
+import deleteUserRoute from './routes/user/delete-user.js'
+// blog route  
+//mport postBlogRoute from './routes/blog/postBlog.js'
+const PORT = process.env.PORT || 3000
+
+//middleware
+// user
+app.use(express.json())
+app.use(addUserRoute)
+app.use(editUserRoute)
+app.use(deleteUserRoute) 
+// blog 
+//app.use(postBlogRoute)
+
+
+
+
+
+
+mongoose.connect(process.env.DBURL)
+.then( () =>{
+    console.log('connected to the database')
+})
+.catch( (err) =>{
+    console.log(err)
+})
+
+
+
+
+
+
+
+app.listen( PORT , () =>{
+    console.log('server is running on port 3000')
+})
